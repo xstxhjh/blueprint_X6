38c3a16f2a56ac964116e9f4fb63baf79e065822
黄嘉豪 <573363596@qq.com>
2023-04-27 00:40:30 +0800

    x6 基本构建node逻辑

--- src/blueprint/main.tsx
+++ src/blueprint/main.tsx
@@ -1,223 +1,269 @@
-import { Graph, Edge, Shape, NodeView, Node } from '@antv/x6'
-import { register } from '@antv/x6-react-shape'
+import { Graph, Edge, Shape, NodeView, Node, Cell } from "@antv/x6";
+import { register } from "@antv/x6-react-shape";
+import { initGraph } from "./graph";
+import "./main.css";
 
-import { initGraph } from './graph'
+const LINE_HEIGHT = 36;
+const NODE_WIDTH = 150;
+const BORDER_WIDTH = 0.5;
 
-const LINE_HEIGHT = 24
-const NODE_WIDTH = 150
+const registerPortNode = () => {
+  const CustomPortComponent = ({ node }: { node: Node }) => {
+    return <div className="custom-port-node">dom A</div>;
+  };
 
-const CustomComponent = ({ node }: { node: Node }) => {
-	return (
-		<div className="custom-port-node">hahaha</div>
-	)
-}
+  const config = {
+    keyName: "custom-port-node",
+    shape: "custom-port-node",
+    component: CustomPortComponent,
+    width: NODE_WIDTH - BORDER_WIDTH * 2,
+    height: LINE_HEIGHT,
+    data: {
+      disableMove: true,
+    },
+    attrs: {
+      root: {
+        magnet: false,
+      },
+      body: {
+        fill: "#EFF4FF",
+        stroke: "#EFF4FF",
+        strokeWidth: 0,
+      },
+    },
+    ports: {
+      groups: {
+        in: {
+          position: {
+            name: "left",
+          },
+          attrs: {
+            portBody: {
+              magnet: "passive",
+              r: 4,
+              stroke: "#5F95FF",
+              fill: "#fff",
+              strokeWidth: 1,
+            },
+          },
+        },
+        out: {
+          position: {
+            name: "right",
+          },
+          attrs: {
+            portBody: {
+              magnet: true,
+              r: 4,
+              fill: "#fff",
+              stroke: "#5F95FF",
+              strokeWidth: 1,
+            },
+          },
+        },
+      },
+    },
+    portMarkup: [
+      {
+        tagName: "circle",
+        selector: "portBody",
+      },
+    ],
+  };
+
+  register({
+    ...config,
+  });
+};
+
+const registerGroupNode = () => {
+  const CustomGroupComponent = ({ node }: { node: Node }) => {
+    return <div className="custom-group-node">Title</div>;
+  };
+
+  const config = {
+    keyName: "custom-group-node",
+    shape: "custom-group-node",
+    component: CustomGroupComponent,
+    width: NODE_WIDTH,
+    height: LINE_HEIGHT,
+    zIndex: 1,
+    attrs: {
+      body: {
+        fill: "#fffbe6",
+        stroke: "#ffe7ba",
+      },
+    },
+  };
+
+  register({
+    ...config,
+  });
+};
 
 const initBluePrint = () => {
-	const graph = initGraph()
+  const graph = initGraph();
 
-	const config = {
-		keyName: 'custom-port-node',
-		shape: 'custom-port-node',
-		component: CustomComponent,
-		width: NODE_WIDTH,
-		height: LINE_HEIGHT,
-		// data: {
-		// 	disableMove: true
-		// },
-		attrs: {
-			root: {
-				magnet: false,
-			},
-			body: {
-				fill: '#EFF4FF',
-				stroke: '#EFF4FF',
-				strokeWidth: 0,
-			},
-		},
-		ports: {
-			items: [
-				{
-					group: 'out',
-				},
-			],
-			groups: {
-				in: {
-					position: {
-						name: 'left',
-					},
-					attrs: {
-						portBody: {
-							magnet: 'passive',
-							r: 4,
-							stroke: '#5F95FF',
-							fill: '#fff',
-							strokeWidth: 1,
-						},
-					},
-				},
-				out: {
-					position: {
-						name: 'right',
-					},
-					attrs: {
-						portBody: {
-							magnet: true,
-							r: 4,
-							fill: '#fff',
-							stroke: '#5F95FF',
-							strokeWidth: 1,
-						},
-					},
-				},
-			},
-		},
-		portMarkup: [
-			{
-				tagName: 'circle',
-				selector: 'portBody',
-			},
-		],
-	}
+  registerPortNode();
 
-	register({
-		...config
-	})
+  registerGroupNode();
 
-	function updateView(view: NodeView) {
-		const cell = view.cell as any
-		if (cell === 'custom-port-node') {
-			cell.getInPorts().forEach((port: any) => {
-				const portNode = view.findPortElem(port.id!, 'portBody')
-				view.unhighlight(portNode, {
-					highlighter: {
-						name: 'stroke',
-						args: {
-							attrs: {
-								fill: '#fff',
-								stroke: '#47C769',
-							},
-						},
-					},
-				})
+  function updateView(view: NodeView) {
+    const cell = view.cell as any;
+    if (cell === "custom-port-node") {
+      cell.getInPorts().forEach((port: any) => {
+        const portNode = view.findPortElem(port.id!, "portBody");
+        view.unhighlight(portNode, {
+          highlighter: {
+            name: "stroke",
+            args: {
+              attrs: {
+                fill: "#fff",
+                stroke: "#47C769",
+              },
+            },
+          },
+        });
+      });
+      cell.updateInPorts(graph);
+    }
+  }
+
+  graph.on("edge:connected", ({ previousView, currentView }) => {
+    if (previousView) {
+      updateView(previousView as NodeView);
+    }
+    if (currentView) {
+      updateView(currentView as NodeView);
+    }
+  });
+
+  graph.on("edge:removed", ({ edge, options }) => {
+    if (!options.ui) {
+      return;
+    }
+
+    const target = edge.getTargetCell() as any;
+    if (target && target.keyName === "custom-port-node") {
+      target.updateInPorts(graph);
+    }
+  });
+
+  graph.on("edge:mouseenter", ({ edge }) => {
+    edge.addTools([
+      "source-arrowhead",
+      "target-arrowhead",
+      {
+        name: "button-remove",
+        args: {
+          distance: -30,
+        },
+      },
+    ]);
+  });
+
+  graph.on("edge:mouseleave", ({ edge }) => {
+    edge.removeTools();
+  });
+
+  const createPortNode = (x: number, y: number, opt: { ports: string[] }) => {
+    // const node = new MyShape()
+    // node.resize(NODE_WIDTH, LINE_HEIGHT).position(x, y).updateInPorts(graph)
+    // graph.addNode(node)
+
+    const node = graph.addNode({
+      x: x + BORDER_WIDTH,
+      y: y,
+      shape: "custom-port-node",
+    }) as any;
+
+    opt.ports.map(name=>{
+			node.addPort({
+				group: name
 			})
-			cell.updateInPorts(graph)
-		}
-	}
+		})
+		
+    node.getInPorts = function () {
+      return this.getPortsByGroup("in");
+    };
 
-	graph.on('edge:connected', ({ previousView, currentView }) => {
-		if (previousView) {
-			updateView(previousView as NodeView)
-		}
-		if (currentView) {
-			updateView(currentView as NodeView)
-		}
-	})
+    node.getOutPorts = function () {
+      return this.getPortsByGroup("out");
+    };
 
-	graph.on('edge:removed', ({ edge, options }) => {
-		if (!options.ui) {
-			return
-		}
+    node.getUsedInPorts = function (graph: Graph) {
+      const incomingEdges = graph.getIncomingEdges(this) || [];
+      return incomingEdges.map((edge: Edge) => {
+        const portId = edge.getTargetPortId();
+        return this.getPort(portId!);
+      });
+    };
 
-		const target = edge.getTargetCell() as any
-		if (target && target.keyName === 'custom-port-node') {
-			target.updateInPorts(graph)
-		}
-	})
+    node.getNewInPorts = function (length: number) {
+      return Array.from(
+        {
+          length,
+        },
+        () => {
+          return {
+            group: "in",
+          };
+        }
+      );
+    };
 
-	graph.on('edge:mouseenter', ({ edge }) => {
-		edge.addTools([
-			'source-arrowhead',
-			'target-arrowhead',
-			{
-				name: 'button-remove',
-				args: {
-					distance: -30,
-				},
-			},
-		])
-	})
+    node.updateInPorts = function (graph: Graph) {
+      const minNumberOfPorts = 1;
+      const ports = this.getInPorts();
+      const usedPorts = this.getUsedInPorts(graph);
+      const newPorts = this.getNewInPorts(
+        Math.max(minNumberOfPorts - usedPorts.length, 1)
+      );
 
-	graph.on('edge:mouseleave', ({ edge }) => {
-		edge.removeTools()
-	})
+      if (
+        ports.length === minNumberOfPorts &&
+        ports.length - usedPorts.length > 0
+      ) {
+        // noop
+      } else if (ports.length === usedPorts.length) {
+        this.addPorts(newPorts);
+      } else if (ports.length + 1 > usedPorts.length) {
+        this.prop(
+          ["ports", "items"],
+          this.getOutPorts().concat(usedPorts).concat(newPorts),
+          {
+            rewrite: true,
+          }
+        );
+      }
 
-	const createBox = (x = 80, y = 40) => {
+      return this;
+    };
 
-		// const node = new MyShape()
-		// node.resize(NODE_WIDTH, LINE_HEIGHT).position(x, y).updateInPorts(graph)
-		// graph.addNode(node)
+    return node;
+  };
 
-		const node = graph.addNode({
-			x: 120,
-			y: 50,
-			shape: 'custom-port-node',
-		}) as any
+  const createNodeGroup = (x = 80, y = 40, data: any) => {
+    const parent = graph.addNode({
+      x: x,
+      y: y,	
+      shape: "custom-group-node",
+    }) as any;
 
-		node.getInPorts = function () {
-			return this.getPortsByGroup('in')
-		}
+    data.map((item: any, index: number) => {
+			console.log(item)
+      const child = createPortNode(x, y + LINE_HEIGHT * (index + 1), {ports: [item]});
+      parent.addChild(child);
+    });
 
-		node.getOutPorts = function () {
-			return this.getPortsByGroup('out')
-		}
+    parent.resize(NODE_WIDTH, LINE_HEIGHT * (data.length + 1));
+  };
+  createNodeGroup(80, 40,['out', 'out','out']);
+  createNodeGroup(300, 40,['in', 'in','in']);
 
-		node.getUsedInPorts = function (graph: Graph) {
-			const incomingEdges = graph.getIncomingEdges(this) || []
-			return incomingEdges.map((edge: Edge) => {
-				const portId = edge.getTargetPortId()
-				return this.getPort(portId!)
-			})
-		}
+  return {
+    createPortNode,
+  };
+};
 
-		node.getNewInPorts = function (length: number) {
-			return Array.from(
-				{
-					length,
-				},
-				() => {
-					return {
-						group: 'in',
-					}
-				},
-			)
-		}
-
-		node.updateInPorts = function (graph: Graph) {
-			const minNumberOfPorts = 1
-			const ports = this.getInPorts()
-			const usedPorts = this.getUsedInPorts(graph)
-			const newPorts = this.getNewInPorts(
-				Math.max(minNumberOfPorts - usedPorts.length, 1),
-			)
-
-			if (
-				ports.length === minNumberOfPorts &&
-				ports.length - usedPorts.length > 0
-			) {
-				// noop
-			} else if (ports.length === usedPorts.length) {
-				this.addPorts(newPorts)
-			} else if (ports.length + 1 > usedPorts.length) {
-				this.prop(
-					['ports', 'items'],
-					this.getOutPorts().concat(usedPorts).concat(newPorts),
-					{
-						rewrite: true,
-					},
-				)
-			}
-
-			return this
-		}
-		console.log(node)
-	}
-
-	createBox()
-
-	return {
-		createBox
-	}
-}
-
-
-export { initBluePrint }
+export { initBluePrint };
